bffc63f66a0de4433e256917b17e887b6025347d
root <root@ip-172-31-26-190.us-east-2.compute.internal>
2021-10-08 09:47:32 +0000

    start

---
+++ startApp.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+export SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTk1MTgzNywiZXhwIjoxOTQ3NTI3ODM3fQ.OVMR_WiGUsuCVylx7Ih6Kuy40LYK-eipFKu7t6qydUE"
+export SUPABASE_URL="https://kgshezreyobypiidaeii.supabase.co"
+cd /home/ubuntu/serverRepos/urlShortener;
+git pull;
+python3 app.py;
