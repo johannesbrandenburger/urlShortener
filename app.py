55e6a919093e9fba4f48a522687103087b811911
Johannes Brandenburger <johannes0709@icloud.com>
2021-10-08 12:14:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def addNewShortCutToDatabase(shortcut, url):
     return pushWasSuccessfull
 
 #########  run app  #########
-app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
