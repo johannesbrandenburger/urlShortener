d77e8a040bc8485c5a05eee6220510dfbac2d469
Johannes Brandenburger <johannes0709@icloud.com>
2021-10-10 21:18:24 +0200

    Update startApp.bash

--- startApp.bash
+++ startApp.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "SCRIPT STARTED AT"
+date
+
 export SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTk1MTgzNywiZXhwIjoxOTQ3NTI3ODM3fQ.OVMR_WiGUsuCVylx7Ih6Kuy40LYK-eipFKu7t6qydUE"
 export SUPABASE_URL="https://kgshezreyobypiidaeii.supabase.co"
 cd /home/ubuntu/serverRepos/urlShortener;
