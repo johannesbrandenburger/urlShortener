a37b5e75dd9902b4785a68b632194cca6102ad3a
Johannes Brandenburger <johannes0709@icloud.com>
2021-12-23 15:22:46 +0100

    deleted google redirect

--- j1bsite/pages/index.js
+++ j1bsite/pages/index.js
@@ -34,7 +34,7 @@ export default class Home extends React.Component {
       });
     }, 1000);
 
-    window.location.href = "https://google.com/";
+    // window.location.href = "https://google.com/";
 
   }
 
